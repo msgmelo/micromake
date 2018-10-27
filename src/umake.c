8ea8c3597739f5b655535b21d4d54864392d78ca
Melody Sangrey Grappo <grappom@LabImage2016-17>
2018-10-26 23:31:54 -0700

    completed umake.c

--- src/umake.c
+++ src/umake.c
@@ -10,6 +10,8 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include "arg_parse.h"
+#include "target.h"
+#include "list.h"
 
 
 /* CONSTANTS */
@@ -18,27 +20,42 @@
 
 /* Process Line
  * line   The command line to execute.
- * This function interprets line as a command line.  It creates a new child
- * process to execute the line and waits for that process to complete. 
+ * This function creates a new child process to execute the line and 
+ * waits for that process to complete. 
  */
-void processline(char* line);
+void processline(char** argv, int argcp);
 
 /* Main entry point.
  * argc    A count of command-line arguments 
  * argv    The command-line argument valus
  *
  * Micro-make (umake) reads from the uMakefile in the current working
- * directory.  The file is read one line at a time.  Lines with a leading tab
- * character ('\t') are interpreted as a command and passed to processline minus
- * the leading tab.
+ * directory.  The file is read one line at a time. Lines are parsed into targets
+ * with associated rules.  All targets and connected rules are parsed. Target's given
+ * by command linehave their rules processed by processline.
+ *
+ * Assumes uMakefile follows standard formatting rules
  */
 int main(int argc, const char* argv[]) {
 
-    FILE* makefile  = fopen("./uMakefile", "r");
+    if (argc < 2) {
+	printf("must name at least one target\n");
+	printf("usage: %s <target_1> <target_2> ... <target_n>\n", argv[0]);
+	exit(1);
+    } 
 
-    size_t  bufsize = 0;
-    char*   line    = NULL;
-    ssize_t linelen = getline(&line, &bufsize, makefile);
+
+    
+    FILE*   makefile  = fopen("./uMakefile", "r");
+
+    size_t      bufsize   = 0;
+    char*       line      = NULL;
+    ssize_t     linelen   = getline(&line, &bufsize, makefile);
+    target*     t         = NULL;
+    target_list l         = NULL;
+    int         count     = 0;
+    char**      rule      = NULL;
+    int         in_target = 0;
 
     while(-1 != linelen) {
 
@@ -47,12 +64,44 @@ int main(int argc, const char* argv[]) {
             line[linelen] = '\0';
         }
 
-	if(line[0] == '\t') 
-            processline(&line[1]);
+	if (is_target(line) && !in_target) {
+	    in_target = 1;
+
+	    char* name = target_parsename(line);
+
+	    t = target_new(name);
+
+	} else if (is_target(line) && in_target) {
+	    
+	    list_append(&l, (cons*) t);
+	    t = target_new(target_parsename(line));
+
+	} else if (in_target && line[0] == '\t') {
+	    
+	    rule = arg_parse(&line[1], &count);
+	    target_addrule(t, rule, count);
+
+	}
 
         linelen = getline(&line, &bufsize, makefile);
 
     }
+
+    list_append(&l, (cons*) t);
+
+    count = 1;
+    while (count < argc) {
+	t = target_findmatch(l, argv[count]);
+
+
+	if (t != NULL) {
+	    char** rules = target_getrules(t);
+            processline(rules, target_getrule_count(t));
+	}
+	count++;
+    }
+
+
 
     fclose(makefile);
     free(line);
@@ -62,32 +111,27 @@ int main(int argc, const char* argv[]) {
 
 
 /* Process Line
- * line   The command line to execute
+ * argv   The command line to execute
+ * argcp  The number of arguments in the command line
  * 
- * This function calls arg_parse to get the arguments for the given line, 
- * then uses fork to execute the command given in the line in the child process.
+ * This function uses fork to execute the command given in argv in the child process.
  * The parent process waits for the child to complete before the function exits.
  */
-void processline (char* line) {
+void processline (char** argv, int argcp) {
  
-    int argcp       = -1;
-    char** argv     = arg_parse(line, &argcp);
-    
-    if (argcp  > 1) {
+    if (argcp  > 0) {
 	const pid_t cpid = fork();
 
 	switch(cpid) {
 
 	  case -1: { // error
 	      perror("fork");
-	      free(argv);
 	      break;
 	  }
 
 	  case 0: { // child
 	      execvp(argv[0], argv);
 	      perror("execvp");
-	      free(argv);
 	      exit(EXIT_FAILURE);
 	      break;
 	  }
@@ -102,9 +146,10 @@ void processline (char* line) {
 		      cpid, pid);
 	      }
 
-	      free(argv); 
 	      break;
 	  }
        } 
     }
 }
+
+
